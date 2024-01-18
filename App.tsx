6e11ac3527d32c6c4749b81ed97c08cf37ca7099
cagatayyildiz <yildiz.cagatay@hotmail.com>
2024-01-18 20:26:55 +0300

    react native paper

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { AuthProvider } from './src/context/AuthContext'
 import Start from './src/Start'
 import { UserProvider } from './src/context/UserContext'
@@ -7,16 +7,18 @@ import ContactsList from './src/reactNativeSamples/ContactsList'
 import AddNewcontact from './src/reactNativeSamples/AddNewcontact'
 import ImageGallery from './src/reactNativeSamples/ImageGallery'
 import ReactNativeReadFile from './src/reactNativeSamples/ReactNativeReadFile'
+import messaging from '@react-native-firebase/messaging';
+import { Button, List, Modal, PaperProvider, Portal } from 'react-native-paper'
 
 
 const App = () => {
 
-  return <SafeAreaView style={{flex:1}}>
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
 
-    <ReactNativeReadFile />
-  
+  return <ModalSample/>
 
-  </SafeAreaView>
 
 
   return (<>
@@ -30,3 +32,68 @@ const App = () => {
 }
 
 export default App
+
+
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
+
+
+
+
+function ProductsScreen() {
+
+  const [products, setproducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://northwind.vercel.app/api/products')
+      .then(response => response.json())
+      .then(json => setproducts(json))
+  })
+
+  return <>
+    <ScrollView>
+      {
+        products.map((product: any) => {
+          return <List.Item
+            title={product.name}
+            description={product.unitPrice}
+            left={props => <List.Icon {...props} icon="folder" />}
+          />
+        })
+      }
+    </ScrollView>
+
+  </>
+
+}
+
+
+
+function ModalSample(){
+
+  const [isShow, setisShow] = useState(false)
+
+  const containerStyle = {backgroundColor: 'white', padding: 20};
+
+
+  return (
+    <PaperProvider>
+      <Portal>
+        <Modal visible={isShow} onDismiss={() => setisShow(false)} contentContainerStyle={containerStyle}>
+          <Text>Example Modal.  Click outside this area to dismiss.</Text>
+        </Modal>
+      </Portal>
+      <Button style={{marginTop: 30}} onPress={() => setisShow(true)}>
+        Show
+      </Button>
+    </PaperProvider>
+  );
+
+}
